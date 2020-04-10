163c34f7217685dd29bf955010e94da150704fbf
Anastasia Oudin <anastasia.oudin@gmail.com>
2020-04-10 13:28:01 +0400

    feat(app): calc number of days between two dates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  dateForm = new FormGroup({
+    start: new FormControl(new Date(), [Validators.required]),
+    end: new FormControl(undefined, [Validators.required]),
+    endIncluded: new FormControl(false)
+  })
+  result: string
+
+  calcDate() {
+    const [start, end] = this.sortTwoDates(
+      dayjs(this.dateForm.value.start), 
+      dayjs(this.dateForm.value.end))
+    const res: number = this.addStartDate(start, end) + 
+      end.diff(start, 'day') +
+      this.addEndDate(this.dateForm.value.endIncluded)
+    this.result = this.getTextResult(res, start, end, this.dateForm.value.endIncluded)
+  }
+
+  private sortTwoDates(date1: dayjs.Dayjs, date2: dayjs.Dayjs): dayjs.Dayjs[] {
+    return date1.isBefore(date2) ?
+      [date1, date2] :
+      [date2, date1]
+  }
+
+  /* start date is not taken into account by dayjs.diff, 
+  include manually if more than one day of difference */
+  private addStartDate(start: dayjs.Dayjs, end: dayjs.Dayjs): number {
+    return end.isSame(start, 'day') ? 0 : 1
+  }
+
+  private addEndDate(isIncluded: boolean): number {
+    return isIncluded ? 1 : 0
+  }
+
+  private getTextResult(result: number, start: dayjs.Dayjs, end: dayjs.Dayjs, endIncluded?: boolean): string {
+    return `
+    <p>From and including: ${start.format('DD/MM/YYYY')}</p>
+    <p>To${endIncluded ? ' and including': ''}: ${end.format('DD/MM/YYYY')}</p>
+    <p>Result: ${result} ${result > 1 ? 'days' : 'day'}</p>
+    <p>It is ${result} ${result > 1 ? 'days' : 'day'} from the start date to the end date${endIncluded ? ', end date included' : '' }.</p>
+    `
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker'; 
+import { DateAdapter, NativeDateAdapter, MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,9 +16,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
-  providers: [],
+  providers: [
+    {provide: DateAdapter, useClass: NativeDateAdapter},
+    {provide: MAT_DATE_LOCALE, useValue: 'en-GB'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
